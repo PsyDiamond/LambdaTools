8776f0885d7382410083d0b90406e182a0f90fb3
Lex Talionis <lextalionis1488@gmail.com>
2019-02-14 19:03:22 +0700

    Убрал лишние ссылки и добавил комментарии

--- Common/RunMethod.cs
+++ Common/RunMethod.cs
@@ -3,10 +3,24 @@ using System.Reflection;
 
 namespace Lextalionis.LambdaTools.Common
 {
+    /// <summary>
+    /// Класс для выполнения метода (с аргументами)
+    /// </summary>
     public class RunMethod
     {
+        /// <summary>
+        /// Метод
+        /// </summary>
         public MethodInfo Method { get; set; }
+        /// <summary>
+        /// Аргументы
+        /// </summary>
         public object[] Arguments { get; set; }
+        /// <summary>
+        /// Тип возвращаемого значения
+        /// </summary>
+// ReSharper disable UnusedAutoPropertyAccessor.Global
         public Type Return { get; set; }
+// ReSharper restore UnusedAutoPropertyAccessor.Global
     }
 }
